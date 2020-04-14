258a2c91acc45b1b95f46008e8e0753c91207861
lin-zhou <zhou.lindsay@yahoo.com>
2020-04-14 00:51:50 -0400

    Moved git ignore & recovered README

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyBspb87rw0-QZeA529PV3CofHkeimWDok8",
+  authDomain: "easyaccess-9ffaa.firebaseapp.com",
+  databaseURL: "https://easyaccess-9ffaa.firebaseio.com",
+  projectId: "easyaccess-9ffaa",
+  storageBucket: "easyaccess-9ffaa.appspot.com",
+  messagingSenderId: "566756662233",
+  appId: "1:566756662233:web:ba5fc14feb2a91276f97c8",
+  measurementId: "G-2FTFKJXW13"
+};
+
+export default config;
